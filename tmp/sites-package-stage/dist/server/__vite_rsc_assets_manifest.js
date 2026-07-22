21daccf0e2fb52f1aa30c538cbfa93daa3d9d831
Huypc <quanghuyy0411@gmail.com>
2026-07-22 12:01:47 +0700

    second

---
+++ tmp/sites-package-stage/dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,47 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-DVmEClTs.js\")",
+  "clientReferenceDeps": {
+    "6efdf509a785": {
+      "js": [
+        "/assets/page-B0sF_yx_.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js",
+        "/assets/index-DVmEClTs.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/assets/index-DVmEClTs.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-DCVQb5Mx.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-DVmEClTs.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-DVmEClTs.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-CXnKph_e.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index--PMAdnG4.css"
+      ]
+    }
+  }
+}
